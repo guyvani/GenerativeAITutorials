c0061802d00e5f24f6be187dbb456d4763b3dce8
GMM <guyvani@users.noreply.github.com>
2023-08-16 09:53:14 -0400

    adding sample apps files

---
+++ Building_Demos_with_Gradio/L3_Image_generation_app.py
@@ -0,0 +1,328 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # L3: Image generation app 🎨
+
+# Load your HF API key and relevant Python libraries
+
+# In[1]:
+
+
+import os
+import io
+import IPython.display
+from PIL import Image
+import base64 
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+hf_api_key = os.environ['HF_API_KEY']
+
+
+# In[2]:
+
+
+# Helper function
+import requests, json
+
+#Text-to-image endpoint
+def get_completion(inputs, parameters=None, ENDPOINT_URL=os.environ['HF_API_TTI_BASE']):
+    headers = {
+      "Authorization": f"Bearer {hf_api_key}",
+      "Content-Type": "application/json"
+    }   
+    data = { "inputs": inputs }
+    if parameters is not None:
+        data.update({"parameters": parameters})
+    response = requests.request("POST",
+                                ENDPOINT_URL,
+                                headers=headers,
+                                data=json.dumps(data))
+    return json.loads(response.content.decode("utf-8"))
+
+
+# ## Building an image generation app 
+
+# Here we are going to run `runwayml/stable-diffusion-v1-5` using the `🧨 diffusers` library.
+
+# ### How about running it locally?
+# The code would look very similar if you were running it locally instead of from an API.
+# ```py
+# from diffusers import DiffusionPipeline
+# 
+# pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+# 
+# def get_completion(prompt):
+#     return pipeline(prompt).images[0]    
+# ```
+
+# In[3]:
+
+
+prompt = "a dog in a park"
+
+result = get_completion(prompt)
+IPython.display.HTML(f'<img src="data:image/png;base64,{result}" />')
+
+
+# ## Generating with `gr.Interface()`
+
+# In[7]:
+
+
+import gradio as gr 
+
+#A helper function to convert the PIL image to base64
+#so you can send it to the API
+def base64_to_pil(img_base64):
+    base64_decoded = base64.b64decode(img_base64)
+    byte_stream = io.BytesIO(base64_decoded)
+    pil_image = Image.open(byte_stream)
+    return pil_image
+
+def generate(prompt):
+    output = get_completion(prompt)
+    result_image = base64_to_pil(output)
+    return result_image
+
+gr.close_all()
+demo = gr.Interface(fn=generate,
+                    inputs=[gr.Textbox(label="Your prompt")],
+                    outputs=[gr.Image(label="Result")],
+                    title="Image Generation with Stable Diffusion",
+                    description="Generate any image with Stable Diffusion",
+                    allow_flagging="never",
+                    examples=["the spirit of a tamagotchi wandering in the city of Vienna","a mecha robot in a favela"])
+
+demo.launch(share=True, server_port=int(os.environ['PORT1']))
+#demo.launch()
+
+
+# In[ ]:
+
+
+demo.close()
+
+
+# ## Building a more advanced interface
+
+# In[8]:
+
+
+import gradio as gr 
+
+#A helper function to convert the PIL image to base64 
+# so you can send it to the API
+def base64_to_pil(img_base64):
+    base64_decoded = base64.b64decode(img_base64)
+    byte_stream = io.BytesIO(base64_decoded)
+    pil_image = Image.open(byte_stream)
+    return pil_image
+
+def generate(prompt, negative_prompt, steps, guidance, width, height):
+    params = {
+        "negative_prompt": negative_prompt,
+        "num_inference_steps": steps,
+        "guidance_scale": guidance,
+        "width": width,
+        "height": height
+    }
+    
+    output = get_completion(prompt, params)
+    pil_image = base64_to_pil(output)
+    return pil_image
+
+gr.close_all()
+demo = gr.Interface(fn=generate,
+                    inputs=[
+                        gr.Textbox(label="Your prompt"),
+                        gr.Textbox(label="Negative prompt"),
+                        gr.Slider(label="Inference Steps", minimum=1, maximum=100, value=25,
+                                 info="In how many steps will the denoiser denoise the image?"),
+                        gr.Slider(label="Guidance Scale", minimum=1, maximum=20, value=7, 
+                                  info="Controls how much the text prompt influences the result"),
+                        gr.Slider(label="Width", minimum=64, maximum=512, step=64, value=512),
+                        gr.Slider(label="Height", minimum=64, maximum=512, step=64, value=512),
+                    ],
+                    outputs=[gr.Image(label="Result")],
+                    title="Image Generation with Stable Diffusion",
+                    description="Generate any image with Stable Diffusion",
+                    allow_flagging="never"
+                    )
+
+demo.launch(share=True, server_port=int(os.environ['PORT2']))
+
+
+# In[ ]:
+
+
+demo.close()
+
+
+# ## `gr.Blocks()` to the rescue!
+
+# In[ ]:
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("# Image Generation with Stable Diffusion")
+    prompt = gr.Textbox(label="Your prompt")
+    with gr.Row():
+        with gr.Column():
+            negative_prompt = gr.Textbox(label="Negative prompt")
+            steps = gr.Slider(label="Inference Steps", minimum=1, maximum=100, value=25,
+                      info="In many steps will the denoiser denoise the image?")
+            guidance = gr.Slider(label="Guidance Scale", minimum=1, maximum=20, value=7,
+                      info="Controls how much the text prompt influences the result")
+            width = gr.Slider(label="Width", minimum=64, maximum=512, step=64, value=512)
+            height = gr.Slider(label="Height", minimum=64, maximum=512, step=64, value=512)
+            btn = gr.Button("Submit")
+        with gr.Column():
+            output = gr.Image(label="Result")
+
+    btn.click(fn=generate, inputs=[prompt,negative_prompt,steps,guidance,width,height], outputs=[output])
+gr.close_all()
+demo.launch(share=True, server_port=int(os.environ['PORT3']))
+
+
+# In[ ]:
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("# Image Generation with Stable Diffusion")
+    with gr.Row():
+        with gr.Column(scale=4):
+            prompt = gr.Textbox(label="Your prompt") #Give prompt some real estate
+        with gr.Column(scale=1, min_width=50):
+            btn = gr.Button("Submit") #Submit button side by side!
+    with gr.Accordion("Advanced options", open=False): #Let's hide the advanced options!
+            negative_prompt = gr.Textbox(label="Negative prompt")
+            with gr.Row():
+                with gr.Column():
+                    steps = gr.Slider(label="Inference Steps", minimum=1, maximum=100, value=25,
+                      info="In many steps will the denoiser denoise the image?")
+                    guidance = gr.Slider(label="Guidance Scale", minimum=1, maximum=20, value=7,
+                      info="Controls how much the text prompt influences the result")
+                with gr.Column():
+                    width = gr.Slider(label="Width", minimum=64, maximum=512, step=64, value=512)
+                    height = gr.Slider(label="Height", minimum=64, maximum=512, step=64, value=512)
+    output = gr.Image(label="Result") #Move the output up too
+            
+    btn.click(fn=generate, inputs=[prompt,negative_prompt,steps,guidance,width,height], outputs=[output])
+
+gr.close_all()
+demo.launch(share=True, server_port=int(os.environ['PORT4']))
+
+
+# In[ ]:
+
+
+gr.close_all()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Building_Demos_with_Gradio/L5_Chat_with_any_LLM.py
@@ -0,0 +1,292 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # L5: Chat with any LLM! 💬
+
+# Load your HF API key and relevant Python libraries
+
+# In[ ]:
+
+
+import os
+import io
+import IPython.display
+from PIL import Image
+import base64 
+import requests 
+requests.adapters.DEFAULT_TIMEOUT = 60
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+hf_api_key = os.environ['HF_API_KEY']
+
+
+# In[ ]:
+
+
+# Helper function
+import requests, json
+from text_generation import Client
+
+#FalcomLM-instruct endpoint on the text_generation library
+client = Client(os.environ['HF_API_FALCOM_BASE'], headers={"Authorization": f"Basic {hf_api_key}"}, timeout=120)
+
+
+# ## Building an app to chat with any LLM!
+
+# Here we'll be using an [Inference Endpoint](https://huggingface.co/inference-endpoints) for `falcon-40b-instruct` , one of best ranking open source LLM on the [🤗 Open LLM Leaderboard](https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard). 
+# 
+# To run it locally, one can use the [Transformers library](https://huggingface.co/docs/transformers/index) or the [text-generation-inference](https://github.com/huggingface/text-generation-inference) 
+
+# In[ ]:
+
+
+prompt = "Has math been invented or discovered?"
+client.generate(prompt, max_new_tokens=256).generated_text
+
+
+# In[ ]:
+
+
+#Back to Lesson 2, time flies!
+import gradio as gr
+def generate(input, slider):
+    output = client.generate(input, max_new_tokens=slider).generated_text
+    return output
+
+demo = gr.Interface(fn=generate, inputs=[gr.Textbox(label="Prompt"), gr.Slider(label="Max new tokens", value=20,  maximum=1024, minimum=1)], outputs=[gr.Textbox(label="Completion")])
+gr.close_all()
+demo.launch(share=True, server_port=int(os.environ['PORT1']))
+
+
+# ## `gr.Chatbot()` to the rescue!
+
+# In[ ]:
+
+
+import random
+
+def respond(message, chat_history):
+        #No LLM here, just respond with a random pre-made message
+        bot_message = random.choice(["Tell me more about it", 
+                                     "Cool, but I'm not interested", 
+                                     "Hmmmm, ok then"]) 
+        chat_history.append((message, bot_message))
+        return "", chat_history
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot(height=240) #just to fit the notebook
+    msg = gr.Textbox(label="Prompt")
+    btn = gr.Button("Submit")
+    clear = gr.ClearButton(components=[msg, chatbot], value="Clear console")
+
+    btn.click(respond, inputs=[msg, chatbot], outputs=[msg, chatbot])
+    msg.submit(respond, inputs=[msg, chatbot], outputs=[msg, chatbot]) #Press enter to submit
+gr.close_all()
+demo.launch(share=True, server_port=int(os.environ['PORT2']))
+
+
+# In[ ]:
+
+
+def format_chat_prompt(message, chat_history):
+    prompt = ""
+    for turn in chat_history:
+        user_message, bot_message = turn
+        prompt = f"{prompt}\nUser: {user_message}\nAssistant: {bot_message}"
+    prompt = f"{prompt}\nUser: {message}\nAssistant:"
+    return prompt
+
+def respond(message, chat_history):
+        formatted_prompt = format_chat_prompt(message, chat_history)
+        bot_message = client.generate(formatted_prompt,
+                                     max_new_tokens=1024,
+                                     stop_sequences=["\nUser:", "<|endoftext|>"]).generated_text
+        chat_history.append((message, bot_message))
+        return "", chat_history
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot(height=240) #just to fit the notebook
+    msg = gr.Textbox(label="Prompt")
+    btn = gr.Button("Submit")
+    clear = gr.ClearButton(components=[msg, chatbot], value="Clear console")
+
+    btn.click(respond, inputs=[msg, chatbot], outputs=[msg, chatbot])
+    msg.submit(respond, inputs=[msg, chatbot], outputs=[msg, chatbot]) #Press enter to submit
+gr.close_all()
+demo.launch(share=True, server_port=int(os.environ['PORT3']))
+
+
+# ### Adding other advanced features
+
+# In[ ]:
+
+
+def format_chat_prompt(message, chat_history, instruction):
+    prompt = f"System:{instruction}"
+    for turn in chat_history:
+        user_message, bot_message = turn
+        prompt = f"{prompt}\nUser: {user_message}\nAssistant: {bot_message}"
+    prompt = f"{prompt}\nUser: {message}\nAssistant:"
+    return prompt
+
+def respond(message, chat_history, instruction, temperature=0.7):
+    prompt = format_chat_prompt(message, chat_history, instruction)
+    chat_history = chat_history + [[message, ""]]
+    stream = client.generate_stream(prompt,
+                                      max_new_tokens=1024,
+                                      stop_sequences=["\nUser:", "<|endoftext|>"],
+                                      temperature=temperature)
+                                      #stop_sequences to not generate the user answer
+    acc_text = ""
+    #Streaming the tokens
+    for idx, response in enumerate(stream):
+            text_token = response.token.text
+
+            if response.details:
+                return
+
+            if idx == 0 and text_token.startswith(" "):
+                text_token = text_token[1:]
+
+            acc_text += text_token
+            last_turn = list(chat_history.pop(-1))
+            last_turn[-1] += acc_text
+            chat_history = chat_history + [last_turn]
+            yield "", chat_history
+            acc_text = ""
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot(height=240) #just to fit the notebook
+    msg = gr.Textbox(label="Prompt")
+    with gr.Accordion(label="Advanced options",open=False):
+        system = gr.Textbox(label="System message", lines=2, value="A conversation between a user and an LLM-based AI assistant. The assistant gives helpful and honest answers.")
+        temperature = gr.Slider(label="temperature", minimum=0.1, maximum=1, value=0.7, step=0.1)
+    btn = gr.Button("Submit")
+    clear = gr.ClearButton(components=[msg, chatbot], value="Clear console")
+
+    btn.click(respond, inputs=[msg, chatbot, system], outputs=[msg, chatbot])
+    msg.submit(respond, inputs=[msg, chatbot, system], outputs=[msg, chatbot]) #Press enter to submit
+gr.close_all()
+demo.queue().launch(share=True, server_port=int(os.environ['PORT4']))
+
+
+# In[ ]:
+
+
+gr.close_all()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
